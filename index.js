5b27625268ef1f862c0be8b554cbce075506a93d
smkisvarday <susannahkisvarday@hotmail.com>
2021-12-01 21:57:21 -0500

    commit1

---
+++ index.js
@@ -0,0 +1,16 @@
+// function addText (text) {
+//      document.getElementById("ellie").innerHTML = "Ellie will play the lead in Matilda this month."
+// }
+
+// let element = document.getElementById(ellie);
+// element.innerHTML += 'Ellie will play the lead in Matilda this month.'
+
+//document.body.innerHTML += "<p>Ellie will play the lead in Matilda this month.</p>"
+document.getElementById("ellie").innerHTML = "Ellie will play the lead in Matilda this month."
+
+// var h1 = document.getElementById("ellie");
+// var text = "Ellie will play the lead in Matilda this month."
+// function addText(text) {
+//     h1.innerHTML = text;
+// }
+// addText(text);
